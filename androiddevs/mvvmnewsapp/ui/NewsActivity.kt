93047430da164c8748c274ddea5af022d0067bce
DREMO <xplendo@gmail.com>
2020-06-15 01:13:25 +0800

    Initial commit

---
+++ androiddevs/mvvmnewsapp/ui/NewsActivity.kt
@@ -0,0 +1,33 @@
+package com.androiddevs.mvvmnewsapp.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.androiddevs.mvvmnewsapp.R
+import com.androiddevs.mvvmnewsapp.db.ArticleDatabase
+import com.androiddevs.mvvmnewsapp.repository.NewsRepository
+import kotlinx.android.synthetic.main.activity_news.*
+
+
+class NewsActivity : AppCompatActivity() {
+
+    lateinit var newsViewModel: NewsViewModel
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_news)
+
+        val newsRepository = NewsRepository(ArticleDatabase(this))
+        val viewModelProviderFactory = NewsViewModelProviderFactory(newsRepository,application)
+        newsViewModel =
+            ViewModelProvider(this, viewModelProviderFactory).get(NewsViewModel::class.java)
+
+//        val appBarConfig= AppBarConfiguration(setOf(R.id.breakingNewsFragment,
+//        R.id.savedNewsFragment,R.id.searchNewsFragment))
+//        setupActionBarWithNavController(newsNavHostFragment.findNavController())
+        bottomNavigationView.setupWithNavController(newsNavHostFragment.findNavController())
+    }
+
+
+}
---
+++ androiddevs/mvvmnewsapp/ui/fragments/ArticleFragment.kt
@@ -0,0 +1,39 @@
+package com.androiddevs.mvvmnewsapp.ui.fragments
+
+import android.os.Bundle
+import android.view.View
+import android.webkit.WebChromeClient
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.navArgs
+import com.androiddevs.mvvmnewsapp.R
+import com.androiddevs.mvvmnewsapp.ui.NewsActivity
+import com.androiddevs.mvvmnewsapp.ui.NewsViewModel
+import com.google.android.material.snackbar.Snackbar
+import kotlinx.android.synthetic.main.fragment_article.*
+
+
+class ArticleFragment : Fragment(R.layout.fragment_article) {
+    private lateinit var viewModel: NewsViewModel
+    private val args: ArticleFragmentArgs by navArgs()
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewModel = (activity as NewsActivity).newsViewModel
+        //get the argument from the generated arg class
+        val article = args.article
+        webView.apply {
+            /** create a new webViewClient to prevent the it
+            from loading directly from the default web browser **/
+            webViewClient = WebViewClient()
+
+            //load the url
+            loadUrl(article.url)
+        }
+        fab.setOnClickListener {
+            viewModel.saveArticle(article)
+            Snackbar.make(view, "Article saved successfully", Snackbar.LENGTH_SHORT).show()
+        }
+
+    }
+}
